ee4c95a8d67f5cc7fdf2e59d8d95f492026a9b05
56kFM <56k.fm3@gmail.com>
2025-10-13 12:08:24 -0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -74,7 +74,7 @@ export async function onRequest(context, next) {
     'style-src \'self\' \'unsafe-inline\'',
     'img-src \'self\' https: data:',
     'media-src \'self\' https:',
-    'frame-src \'self\' https: https://www.youtube-nocookie.com https://player.vimeo.com https://w.soundcloud.com https://bandcamp.com https://open.spotify.com https://embed.music.apple.com https://comments.app https://t.me',
+    'frame-src \'self\' https: https://www.youtube-nocookie.com https://www.youtube.com https://player.vimeo.com https://open.spotify.com https://w.soundcloud.com https://bandcamp.com https://embed.music.apple.com https://comments.app https://t.me',
     'connect-src \'self\' https: https://*.telegram.org https://*.telegram-cdn.org',
     'frame-ancestors \'none\'',
     'upgrade-insecure-requests',
